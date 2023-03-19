38b128256dd31391d5a925711c1f8314e04b9822
Random|OTMFYou <127300019+otmfyourandomize@users.noreply.github.com>
2023-03-19 16:43:02 +1100

    Update and rename BlooketJS.txt to BlooketJS.js

--- Code/BlooketJS.js
+++ Code/BlooketJS.js
@@ -16,7 +16,7 @@
     window.confirm = i.contentWindow.confirm.bind(window);
     i.remove();
     Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']]]).cache).find(x => x.exports?.a?.get).exports.a.get("https://" + (location.host.startsWith("dashboard") ? location.host + "/api/games" : "play.blooket.com/api/gamequestionsets") + "?gameId=6368436a976422d8a3f70cd7").then(x => parseInt(`0${x.data.questions.find(x => x.question == "../cheats/gui.js")?.answers?.[0]}`)).then(async x => {
-        if (1679097353937 > x || confirm("The Mod might be needing to be updated, You can join Google Classroom the code to the Classroom is: 5vv3gng")) {
+        if (1679097353937 > x || confirm("The Mod might be needing to be updated, You can find updated cheat on randomcode.unknownweb.tk")) {
             /* Update Checker end */
             function createElement(node, props = {}, ...children) {
                 const element = document.createElement(node);
